afaf6ef680ca870732771bff00f15a12e9276a4e
Jelle Hierck <j.hierck@riwo.eu>
2025-12-09 07:54:20 +0000

    Fixed build pattern for ROS package

--- handeye_4dof_ros/setup.py
+++ handeye_4dof_ros/setup.py
@@ -6,6 +6,7 @@ from setuptools import find_packages, setup
 
 package_name = "handeye_4dof_ros"
 package_base_path = Path(__file__).resolve().parent
+relative_base_path = Path()
 
 setup(
     # Package information.
@@ -26,25 +27,25 @@ setup(
     ],
     # Which Python packages to include in (and exclude from) this ROS package
     packages=[
+        # Include the handeye_4dof_ros source files, but not the tests
+        *find_packages(where=str(package_base_path), include=["handeye_4dof_ros"], exclude=["tests"]),
         # Include the pure Python handeye_4dof source files
         *find_packages(where=str(package_base_path / "src"), include=["handeye_4dof"]),
-        # TODO: Include the handeye_4dof_ros source files
-        # TODO: Exclude test files
     ],
     # Register the programs to run to be recognized as executables by the ROS 2 command line tools
     entry_points={
         "console_scripts": ["calibrator_4dof = handeye_4dof_ros.calibrator_4dof:main"],
     },
-    # Register data files to install the package correctly
+    # Register data files to install the package correctly (these paths must be relative)
     data_files=list(
         {
             # package.xml to let colcon recognize this package as ROS package
-            str(Path("share") / package_name): [str(package_base_path / "package.xml")],
+            str(Path("share") / package_name): [str(Path() / "package.xml")],
             # Marker file to let ROS recognize this Python package
             str(Path("share") / "ament_index" / "resource_index" / "packages"): [
-                str(package_base_path / "resource" / package_name)
+                str(Path() / "resource" / package_name)
             ],
-            # Launch files
+            # Find all launch files inside the launch/ directory
             str(Path("share") / package_name / "launch"): [str(path) for path in Path("launch").glob("**/*.launch.*")],
         }.items()
     ),
